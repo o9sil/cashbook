41b1b59ab6945531e15415599323332c93679a85
GDJ26 <GDJ26@DESKTOP-0B7G2R0>
2020-05-19 17:59:21 +0900

    [최지선] 일자별 수입, 지출 합계 출력 / 월별 목록 폼 생성(데이터는 X) / 가계부 삭제 기능 추가
    1. HTML
    	getCashListByDate.html
    		가계부 삭제 링크 생성(클릭시 바로 삭제)
    		이전일, 다음일 링크 생성
    		전체 소비, 수입 합계 금액 생성
    	getCashListByMonth.html
    		월별 달력 기본 틀 생성
    
    2. Controller
    	CashController.java
    		getCashListByDate Get
    			회원별 일자별 입출금 폼 호출
    			현재 날짜 받아서 수입, 지출 리스트 생성
    			전체 합계 금액도 생성 후 폼 호출
    		removeCash Get
    			회원이 특정 가계부 목록을 삭제시 DB에서 데이터를 지워주는 기능 담당
    		getCashListByMonth Get
    			회원별 월별 수입 지출 폼 호출
    			이전, 다음달 링크만 생성
    
    3. Mapper
    	CashMapper
    		deleteCashOne
    			가게부 내역 삭제 SQL 쿼리 생성
    		selectCashKindSum
    			사용자의 특정 일자별 수입, 지출의 합계 총액 받아오는 쿼리 생성
    
    4. Service
    	CashService
    		removeCashOne
    			가계부 내역 1행 삭제
    		getCashListByDate
    			기존 cashList(가계부 내역)만 받아온 것 에서 일자별 금액 합계도 같이 받아오도록 수정

--- cashbook/src/main/java/com/gdu/cashbook/controller/CashController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/CashController.java
@@ -1,11 +1,16 @@
 package com.gdu.cashbook.controller;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.time.Period;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Map;
+
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,8 +25,56 @@ public class CashController {
 	
 	@Autowired private CashService cashService;
 	
+	@GetMapping("/getCashListByMonth")
+	public String getCashListByMonth(HttpSession session, Model model, @RequestParam(value = "yearMonth", required = false) @DateTimeFormat(pattern = "yyyy-MM") YearMonth yearMonth) {		
+		if(session.getAttribute("loginMember") == null) {
+			return "redirect:/";
+		}
+		
+		//이전, 다음달 넘길수있도록
+		model.addAttribute("yearMonthBefore", yearMonth.minus(Period.ofMonths(1)));
+		model.addAttribute("yearMonth", yearMonth);
+		model.addAttribute("yearMonthAfter", yearMonth.plus(Period.ofMonths(1)));
+		
+//		System.out.println(yearMonth.lengthOfMonth());
+//		
+//		//매달 1일, 마지막일
+//		String firstDate = yearMonth + "-01";
+//		String lastDate = yearMonth + "-" + yearMonth.lengthOfMonth();
+//		
+//		LocalDate firstLocalDate = LocalDate.parse(firstDate, DateTimeFormatter.ISO_DATE);
+//		LocalDate lastLocalDate = LocalDate.parse(lastDate, DateTimeFormatter.ISO_DATE);
+//		System.out.println(firstLocalDate.getDayOfWeek());	//MONDAY
+//		System.out.println(lastLocalDate.getDayOfWeek());	//TUESDAY
+		
+		
+		return "getCashListByMonth";
+	}
+	
+	//회원별 입출금 내역 리스트 삭제
+	@GetMapping("/removeCashOne")
+	public String removeCashOne(HttpSession session, @RequestParam(value = "cashNo") int cashNo) {
+		if(session.getAttribute("loginMember") == null) {
+			return "redirect:/";
+		}
+		LoginMember loginMember = null;
+		if(session.getAttribute("loginMember") instanceof LoginMember) {
+			loginMember = (LoginMember)session.getAttribute("loginMember");			
+		}
+		
+		Cash cash = new Cash();
+		cash.setCashNo(cashNo);
+		cash.setMemberId(loginMember.getMemberId());
+		
+		cashService.removeCashOne(cash);
+		
+		return "redirect:/getCashListByDate";
+	}
+	
+	
+	//회원별 일자별 입출금 내역 리스트
 	@GetMapping("/getCashListByDate")
-	public String getCashListByDate(HttpSession session, Model model, @RequestParam(value = "date", defaultValue = "today") String date) {
+	public String getCashListByDate(HttpSession session, Model model, @RequestParam(value = "day", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate day) {
 		if(session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
@@ -29,35 +82,30 @@ public class CashController {
 		LoginMember loginMember = null;
 		if(session.getAttribute("loginMember") instanceof LoginMember) {
 			loginMember = (LoginMember)session.getAttribute("loginMember");			
+		}		
+	
+		//값 안들어오면 오늘날짜
+		if(day == null) {
+			day = LocalDate.now();			
 		}
 		
-		LocalDate cashDate = LocalDate.now();
-		if(!date.equals("today")) {
-			cashDate = LocalDate.parse(date);			
-		}
+		model.addAttribute("day", day);
 		
+		// 년,월만 추출
+		YearMonth yearMonth = YearMonth.from(day);
+		model.addAttribute("yearMonth", yearMonth);
 		
-		
-		model.addAttribute("cashDate", cashDate);
 		
 		Cash cash = new Cash();
 		cash.setMemberId(loginMember.getMemberId());
-		cash.setCashDate(cashDate);
+		cash.setCashDate(day);
 		
-		List<Cash> cashList = cashService.getCashListByDate(cash);
-		model.addAttribute("cashList", cashList);
-
-		//일자별 수입, 지출 합계
-		int cashSum = 0;
-		for(Cash c : cashList) {
-			if(c.getCashKind().equals("수입")) {
-				cashSum += c.getCashPrice();
-			}else if(c.getCashKind().equals("지출")) {
-				cashSum -= c.getCashPrice();
-			}
-		}
+		Map<String, Object> map = cashService.getCashListByDate(cash);
 		
-		model.addAttribute("cashSum", cashSum);
+		model.addAttribute("cashList", map.get("cashList"));
+		
+		//일자별 수입, 지출 합계	
+		model.addAttribute("cashKindSum", map.get("cashKindSum"));
 		
 		return "getCashListByDate";
 	}
--- cashbook/src/main/java/com/gdu/cashbook/mapper/CashMapper.java
+++ cashbook/src/main/java/com/gdu/cashbook/mapper/CashMapper.java
@@ -8,6 +8,13 @@ import com.gdu.cashbook.vo.Cash;
 
 @Mapper
 public interface CashMapper {
+	
+	//가계부 내역 삭제
+	public int deleteCashOne(Cash cash);
+	
+	//일자별 금액 합계 구하기
+	public int selectCashKindSum(Cash cash);
+	
 	//로그인 사용자의 특정날짜 cash 목록
 	public List<Cash> selectCashListByDate(Cash cash);
 }
