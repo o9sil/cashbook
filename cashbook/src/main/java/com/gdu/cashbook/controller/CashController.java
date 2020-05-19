d971680c235be523c476e5a1d26422792c89910f
GDJ26 <GDJ26@DESKTOP-0B7G2R0>
2020-05-19 13:55:23 +0900

    [최지선] 가계부 날짜 입력, 일자별 총액 확인
    1. CashController.java
    	LocalDate 사용 현재날짜 / 받아온 날짜 확인
    	일자별 수입,지출 합계 확인 가능
    
    2. getCashListByDate.html
    	이전, 다음 날짜로 이동 가능(LocalDate 사용)
    	하단에 일자별 총액 확인 가능

--- cashbook/src/main/java/com/gdu/cashbook/controller/CashController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/CashController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.gdu.cashbook.service.CashService;
 import com.gdu.cashbook.vo.Cash;
@@ -20,7 +21,7 @@ public class CashController {
 	@Autowired private CashService cashService;
 	
 	@GetMapping("/getCashListByDate")
-	public String getCashListByDate(HttpSession session, Model model) {
+	public String getCashListByDate(HttpSession session, Model model, @RequestParam(value = "date", defaultValue = "today") String date) {
 		if(session.getAttribute("loginMember") == null) {
 			return "redirect:/";
 		}
@@ -29,19 +30,34 @@ public class CashController {
 		if(session.getAttribute("loginMember") instanceof LoginMember) {
 			loginMember = (LoginMember)session.getAttribute("loginMember");			
 		}
-				
-		LocalDate today = LocalDate.now();
+		
+		LocalDate cashDate = LocalDate.now();
+		if(!date.equals("today")) {
+			cashDate = LocalDate.parse(date);			
+		}
+		
+		
+		
+		model.addAttribute("cashDate", cashDate);
 		
 		Cash cash = new Cash();
 		cash.setMemberId(loginMember.getMemberId());
-		cash.setCashDate(today);
+		cash.setCashDate(cashDate);
 		
 		List<Cash> cashList = cashService.getCashListByDate(cash);
 		model.addAttribute("cashList", cashList);
+
+		//일자별 수입, 지출 합계
+		int cashSum = 0;
+		for(Cash c : cashList) {
+			if(c.getCashKind().equals("수입")) {
+				cashSum += c.getCashPrice();
+			}else if(c.getCashKind().equals("지출")) {
+				cashSum -= c.getCashPrice();
+			}
+		}
 		
-//		for(Cash c : cashList) {
-//			System.out.println(c);
-//		}
+		model.addAttribute("cashSum", cashSum);
 		
 		return "getCashListByDate";
 	}
